ee803579ca96e5282e69975e66acc184c5229ca5
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-12 23:00:28 -0400

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -221,7 +221,7 @@ class CarInterface(CarInterfaceBase):
     ret = self.CS.update(self.cp, self.cp2, self.cp_cam)
     ret.canValid = self.cp.can_valid and self.cp2.can_valid and self.cp_cam.can_valid
 
-    self.CP.enableCruise = self.CC.longcontrol == 0
+    self.CP.enableCruise = not self.CC.longcontrol
 
     # most HKG cars has no long control, it is safer and easier to engage by main on
 #    if not self.CP.openpilotLongitudinalControl:
