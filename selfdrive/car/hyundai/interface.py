94caf210ff4fcd19e4609c40e8d8c4271dd53ec8
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-18 16:16:46 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -43,7 +43,7 @@ class CarInterface(CarInterfaceBase):
 
     #Long tuning Params -  make individual params for cars, baseline Hyundai genesis
     ret.longitudinalTuning.kpBP = [0., 1., 10., 35.]
-    ret.longitudinalTuning.kpV = [.5, 1., 1., 1.]
+    ret.longitudinalTuning.kpV = [3., 2.5, 2.5, 2.5]
     ret.longitudinalTuning.kiBP = [0., 15., 35.]
     ret.longitudinalTuning.kiV = [.96, .96, .95]
     ret.longitudinalTuning.deadzoneBP = [0., .5]
