e37ab2d6fc57456dee0bf6de1a881b2cbb9161d9
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-18 15:58:13 -0400

    update

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -95,8 +95,9 @@ class CarController():
       self.lead_visible = lead_visible
 
     # gas and brake
-    apply_accel, self.accel_steady = accel_hysteresis(apply_accel, self.accel_steady)
+    apply_accel = actuators.gas - actuators.brake
 
+    apply_accel, self.accel_steady = accel_hysteresis(apply_accel, self.accel_steady)
     apply_accel = clip(apply_accel * ACCEL_SCALE, ACCEL_MIN, ACCEL_MAX)
 
     # Steering Torque
