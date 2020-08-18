df6ab902a3d72b5e2a596738f0b3e4f22eea2e77
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-18 11:56:35 -0400

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -129,7 +129,7 @@ class LongControl():
       factor = 1
       if hasLead:
         factor = interp(dRel,[2.0,3.0,4.0,5.0,6.0,7.0,8.0], [5.0,2.5,1.0,0.5,0.25,0.05,0.0])
-      if not CS.standstill or output_gb > -BRAKE_STOPPING_TARGET:
+      if output_gb > -BRAKE_STOPPING_TARGET:
         output_gb -= STOPPING_BRAKE_RATE / RATE * factor
       output_gb = clip(output_gb, -brake_max, gas_max)
 
