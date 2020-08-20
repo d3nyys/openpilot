86e3b051c5fc78b066c9378e01fccc6ae501c131
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-20 16:38:37 -0400

    Update radar_interface.py

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -54,7 +54,7 @@ class RadarInterface(RadarInterfaceBase):
 
     valid = cpt["SCC11"]['ACC_ObjStatus']
     if valid:
-      for ii in range(20):
+      for ii in range(100):
         if ii not in self.pts:
           self.pts[ii] = car.RadarData.RadarPoint.new_message()
           self.pts[ii].trackId = self.track_id
