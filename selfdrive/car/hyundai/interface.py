ca1d3bcf99ef39c23c7df2c86104952de4ebf598
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-20 14:50:25 -0400

    increase long pi by double

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -43,9 +43,9 @@ class CarInterface(CarInterfaceBase):
 
     #Long tuning Params -  make individual params for cars, baseline Hyundai genesis
     ret.longitudinalTuning.kpBP = [0., 1., 10., 35.]
-    ret.longitudinalTuning.kpV = [0.85, 1.3, .85, .65]
+    ret.longitudinalTuning.kpV = [1.7, 2.6, 1.7, 1.3]
     ret.longitudinalTuning.kiBP = [0., 15., 35.]
-    ret.longitudinalTuning.kiV = [.15, .10, .065]
+    ret.longitudinalTuning.kiV = [.30, .20, .13]
     ret.longitudinalTuning.deadzoneBP = [0., .5]
     ret.longitudinalTuning.deadzoneV = [0.00, 0.00]
     ret.gasMaxBP = [0., 1., 1.1, 15., 40.]
