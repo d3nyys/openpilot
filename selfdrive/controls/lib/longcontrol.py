edbee9865c461baaf1063b41780b566c0e09f1f0
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-19 20:56:50 -0400

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -60,7 +60,7 @@ class LongControl():
     self.long_control_state = LongCtrlState.off  # initialized to off
 
     kdBP = [0., 16., 35.]
-    kdV = [0.08, 1.215, 2.51]
+    kdV = [0.008, .1215, .251]
 
     self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                              (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
