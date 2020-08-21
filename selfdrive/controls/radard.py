38a8e19de7a59aee84fdd5263d78a573de141a48
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-21 14:59:29 -0400

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -166,7 +166,7 @@ class RadarD():
     dat.radarState.radarErrors = list(rr.errors)
     dat.radarState.controlsStateMonoTime = sm.logMonoTime['controlsState']
 
-    if has_radar:
+    if has_radar or (1==1):
       dat.radarState.leadOne = get_lead(self.v_ego, self.ready, clusters, sm['model'].lead, low_speed_override=True)
       if dat.radarState.leadOne.status:
         dat.radarState.leadOne.dRel = 150
